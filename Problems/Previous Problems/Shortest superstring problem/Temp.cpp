87fb23f5f6c118b9354a08410db55fbc6926ff53
Mridul <mridul15gupta@gmail.com>
2020-05-14 19:13:07 +0530

    Shortest Superstring problem added

---
+++ Problems/Previous Problems/Shortest superstring problem/Temp.cpp
@@ -0,0 +1,25 @@
+int arrange(string A, int B) 
+{
+    int n=A.length();
+    
+    int dp[n+1][B+1];
+    for(int i=0;i<=n;i++) for(int j=0;j<=B;j++) dp[i][j]=1e9;
+    dp[0][0]=0;
+    
+    for(int j=1;j<=B;j++)   //stable
+    {  
+        for(int i=1;i<=n;i++)   //horse number
+        {
+            int b=0,w=0;
+            for(int k=i;k>0;k--)
+            {
+                b+=A[k-1]=='B';
+                w+=A[k-1]=='W';
+                dp[i][j]=min(dp[i][j],b*w+dp[k-1][j-1]);
+            }
+        }
+    }
+    
+    if(dp[n][B]!=1e9) return dp[n][B];
+    return -1;  
+}
